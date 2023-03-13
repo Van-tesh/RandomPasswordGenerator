1fb4da037d676bdc0120cdcbc51f2a4afafd9128
Van-tesh <smalletvicky@gmail.com>
2023-03-14 00:54:59 +0300

    random password Genarator

---
+++ main.py
@@ -0,0 +1,9 @@
+import random 
+letters= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+Symbols= ['!', '@' ,'#', '$', '%', '&', '*', '+']
+
+print ("Welcome to RandomPasswordGenerator")
+nr_letters= int(input("How many letters would you like in your paswword? \n"))
+nr_symbols= int(input("How many symbols would you like in your paswword? \n"))
+nr_numbers= int(input("How many numbers would you like in your paswword? \n"))
